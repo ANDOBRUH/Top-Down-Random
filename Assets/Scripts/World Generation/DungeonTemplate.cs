3b930b4424b9c7022acd108097c5ea20045a5a31
ANDOBRUH <122355264+ANDOBRUH@users.noreply.github.com>
2024-04-15 09:44:42 +0200

    rgeg

---
+++ Assets/Scripts/World Generation/DungeonTemplate.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DungeonTemplate : MonoBehaviour
+{
+     public GameObject[] topRooms;
+     public GameObject[] bottomRooms;
+     public GameObject[] leftRooms;
+     public GameObject[] rightRooms;
+}
---
+++ Assets/Scripts/World Generation/RoomSpawner.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int openingDirection;
+    // 1 = top
+    // 2 = bottom
+    // 3 = left
+    // 4 = right
+
+    private DungeonTemplate templates;
+    private int random;
+    public bool spawned = false;
+
+    private void Start()
+    {
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<DungeonTemplate>();
+        Invoke("Spawn", 0.1f);
+    }
+
+    void Spawn()
+    {
+        if (spawned == false)
+        {
+            if (openingDirection == 1)
+            {
+                random = Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[random], transform.position, Quaternion.identity);
+            }
+            else if (openingDirection == 2)
+            {
+                random = Random.Range(0, templates.bottomRooms.Length);
+                Instantiate(templates.bottomRooms[random], transform.position, Quaternion.identity);
+            }
+            else if (openingDirection == 3)
+            {
+                random = Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[random], transform.position, Quaternion.identity);
+            }
+            else if (openingDirection == 4)
+            {
+                random = Random.Range(0, templates.rightRooms.Length);
+                Instantiate(templates.rightRooms[random], transform.position, Quaternion.identity);
+            }
+        }
+        spawned = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Spawn Point") && other.GetComponent<RoomSpawner>().spawned == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
