3475d881c80f7d86ea5165041ae8edcc578b70e7
ANDOBRUH <122355264+ANDOBRUH@users.noreply.github.com>
2024-05-21 16:51:31 +0200

    ef

--- Assets/Scripts/DebugWorld.cs
+++ Assets/Scripts/DebugWorld.cs
@@ -6,6 +6,7 @@ public class DebugWorld : MonoBehaviour
 {
     public bool ShowBlackBoxes;
     private GameObject[] blackBoxes;
+    private SpriteRenderer[] blackBoxesSprite;
    
     void Update()
     {
@@ -15,20 +16,23 @@ public class DebugWorld : MonoBehaviour
     private void ActivateBlackBoxes()
     {
         blackBoxes = GameObject.FindGameObjectsWithTag("Black Out");
+        for (int i = 0; i < blackBoxes.Length; i++)
+        {
+            blackBoxesSprite[i] = blackBoxes[i].GetComponent<SpriteRenderer>();
+        }
 
         if (ShowBlackBoxes)
         {
             for (int i = 0; i < blackBoxes.Length; i++)
-            {
-                blackBoxes[i].SetActive(true);
-                Debug.Log("hwedhbsi");
+            {              
+                blackBoxesSprite[i].color = new Color(0, 0, 0, 255);
             }
         }
         else if (!ShowBlackBoxes)
         {
             for (int i = 0; i < blackBoxes.Length; i++)
-            {
-                blackBoxes[i].SetActive(false);
+            {             
+                blackBoxesSprite[i].color = new Color(0, 0, 0, 0);
             }
         }
     }
